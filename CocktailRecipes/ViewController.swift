08950975e45350650b241c6cfb4a9b255a7b3b25
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-27 21:03:37 +0300

    Initial Commit

---
+++ CocktailRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailRecipes
+//
+//  Created by Nikolai Maksimov on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
